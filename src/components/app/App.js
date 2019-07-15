0023fbf7fd0eac25e23a755c6ef0926da7a1f67e
LexiosAlex <lexiosma@gmail.com>
2019-07-15 21:21:21 +0300

    revorked landing pag via ant table, and sidebar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,169 +1,252 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Layout, Row, Col, Button} from 'antd';
+import { Layout, Row, Col, Button, Table} from 'antd';
 import Category from '../category/Category.js'
 
 const ButtonGroup = Button.Group;
-const { Header, Footer, Content } = Layout;
+const { Header, Footer, Content, Sider} = Layout;
+
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Название товара',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a href="javascript:;">{text}</a>,
+  },
+  {
+    title: 'Цена / закуп',
+    dataIndex: 'wholePrice',
+    key: 'wholePrice',
+  },
+  {
+    title: 'Цена',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: '',
+    key: 'action',
+    render: () => (
+      <Row type='flex' justify='space-around'>
+        <Button className="page-main__edit-category-btn" type="primary">
+          Удалить
+        </Button>
+        <Button className="page-main__edit-category-btn" type="primary">
+          Изменить
+        </Button>
+      </Row>
+    ),
+  },
+];
+
+const data = [
+  {
+    categoryName: 'Мясо',
+    categoryTitle: 'meat',
+    items: [
+      {
+        key: '1',
+        id: '1',
+        name: 'Товар 1',
+        wholePrice: '2200',
+        price: '2500',
+        category: 'meat',
+      },
+      {
+        key: '2',
+        id: '2',
+        name: 'Товар 2',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'meat'
+      },
+      {
+        key: '3',
+        id: '3',
+        name: 'Товар 3',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'meat'
+      },
+      {
+        key: '4',
+        id: '4',
+        name: 'Товар 4',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'meat'
+      },
+    ]
+  },
+  {
+    categoryName: 'Рыба',
+    categoryTitle: 'fish',
+    items: [
+      {
+        key: '1',
+        id: '1',
+        name: 'Товар 1',
+        wholePrice: '2200',
+        price: '2500',
+        category: 'fish',
+      },
+      {
+        key: '2',
+        id: '2',
+        name: 'Товар 2',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'fish',
+      },
+      {
+        key: '3',
+        id: '3',
+        name: 'Товар 3',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'fish',
+      },
+      {
+        key: '4',
+        id: '4',
+        name: 'Товар 4',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'fish',
+      },
+    ]
+  },
+  {
+    categoryName: 'Овощи',
+    categoryTitle: 'vegetables',
+    items: [
+      {
+        key: '1',
+        id: '1',
+        name: 'Товар 1',
+        wholePrice: '2200',
+        price: '2500',
+        category: 'vegetables',
+      },
+      {
+        key: '2',
+        id: '2',
+        name: 'Товар 2',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'vegetables',
+      },
+      {
+        key: '3',
+        id: '3',
+        name: 'Товар 3',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'vegetables',
+      },
+      {
+        key: '4',
+        id: '4',
+        name: 'Товар 4',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'vegetables',
+      },
+    ]
+  },
+  {
+    categoryName: 'Фрукты',
+    categoryTitle: 'fruits',
+    items: [
+      {
+        key: '1',
+        id: '1',
+        name: 'Товар 1',
+        wholePrice: '2200',
+        price: '2500',
+        category: 'fruits',
+      },
+      {
+        key: '2',
+        id: '2',
+        name: 'Товар 2',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'fruits',
+      },
+      {
+        key: '3',
+        id: '3',
+        name: 'Товар 3',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'fruits',
+      },
+      {
+        key: '4',
+        id: '4',
+        name: 'Товар 4',
+        wholePrice: '2200',
+        price: '2700',
+        category: 'fruits',
+      },
+    ]
+  },
+  {
+    categoryName: 'Без категории',
+    categoryTitle: 'noCategory',
+    items: [
+
+    ]
+  },
+];
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Header  style={{height:'auto'}} className='main-header'>
-          <Row gutter={20}>
-            <Col span={4}>
-              <a className='main-header__logo logo'>
-                <p className='logo__name'>My-app</p>
-              </a>
-            </Col>
-            <Col span={8} push={4}>
-              <div style={{paddingTop:'50px'}} className='main-header__button-container'>
-                <ButtonGroup>
-                  <Button className="button-header" type="primary">
-                    Добавить товар
-                  </Button>
-                  <Button className="button-header" type="primary">
-                    Добавить категорию
-                  </Button>
-                </ButtonGroup>
-              </div>
-            </Col>
-          </Row>
-        </Header>
-        <Content className="page-main">
-          <Row className='page-main__row' align={'bottom'} gutter={20}>
-            <Col span={4}>
-              <p className='visually-hidden'>Категории</p>
-            </Col>
-            <Col span={2}>
-              <p className='page-main__nav-item page-main__nav-item--id'>id</p>
-            </Col>
-            <Col span={6}>
-              <p className='page-main__nav-item'>Название товара</p>
-            </Col>
-            <Col span={3}>
-              <p className='page-main__nav-item'>Цена/закуп</p>
-            </Col>
-            <Col span={3}>
-              <p className='page-main__nav-item'>Цена</p>
-            </Col>
-            <Col span={6}>
-              <p className='visually-hidden'>Редактировать</p>
-            </Col>
-          </Row>
-          <Row className='page-main__row' gutter={20}>
-            <Col span={4}>
-              <Category/>
-            </Col>
-            <Col style={{textAlign:'center'}} span={2}>
-              <Row className='item-id'>1</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row className='item-name'>Товар 1</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-whole-price'>2200</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-price'>2500</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row type='flex' justify='space-around'>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Удалить
-                </Button>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Изменить
-                </Button>
+        <Layout>
+          <Header  style={{height:'auto'}} className='main-header'>
+            <Row gutter={20}>
+              <Col span={4}>
+                <a className='main-header__logo logo'>
+                  <p className='logo__name'>My-app</p>
+                </a>
+              </Col>
+              <Col span={12} push={4}>
+                <div style={{paddingTop:'50px'}} className='main-header__button-container'>
+                  <ButtonGroup>
+                    <Button className="button-header" type="primary">
+                      Добавить товар
+                    </Button>
+                    <Button className="button-header" type="primary">
+                      Добавить категорию
+                    </Button>
+                  </ButtonGroup>
+                </div>
+              </Col>
+            </Row>
+          </Header>
+          <Layout className='page-main'>
+            <Sider className='page-main__sidebar' style={{background:'#f0f2f5'}}>
+              <Row className='page-main__row' align={'bottom'} gutter={20}>
+                <p className='visually-hidden'>Категории</p>
               </Row>
-            </Col>
-          </Row>
-          <Row className='page-main__row' gutter={20}>
-            <Col span={4}>
-              <Category/>
-            </Col>
-            <Col style={{textAlign:'center'}} span={2}>
-              <Row className='item-id'>2</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row className='item-name'>Товар 2</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-whole-price'>2200</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-price'>2700</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row type='flex' justify='space-around'>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Удалить
-                </Button>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Изменить
-                </Button>
-              </Row>
-            </Col>
-          </Row>
-          <Row className='page-main__row' gutter={20}>
-            <Col span={4}>
-              <Category/>
-            </Col>
-            <Col style={{textAlign:'center'}} span={2}>
-              <Row className='item-id'>3</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row className='item-name'>Товар 3</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-whole-price'>2200</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-price'>2700</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row type='flex' justify='space-around'>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Удалить
-                </Button>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Изменить
-                </Button>
-              </Row>
-            </Col>
-          </Row>
-          <Row className='page-main__row' gutter={20}>
-            <Col span={4}>
-              <Category/>
-              <Button className='category-extra-btn' type={'link'}>Без категории</Button>
-            </Col>
-            <Col style={{textAlign:'center'}} span={2}>
-              <Row className='item-id'>4</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row className='item-name'>Товар 4</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-whole-price'>2200</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={3}>
-              <Row className='item-price'>2700</Row>
-            </Col>
-            <Col style={{textAlign:'center'}} span={6}>
-              <Row type='flex' justify='space-around'>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Удалить
-                </Button>
-                <Button className="page-main__edit-category-btn" type="primary">
-                  Изменить
-                </Button>
-              </Row>
-            </Col>
-          </Row>
-        </Content>
-        <Footer></Footer>
+              {data.map((it, i) => <Category key={`city-${i}`} category={it}/>)}
+            </Sider>
+            <Content>
+              <Table columns={columns} dataSource={data[0].items} />
+            </Content>
+          </Layout>
+          <Footer></Footer>
+        </Layout>
       </div>
     );
   }
--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -3,17 +3,29 @@ import './Category.css';
 import 'antd/dist/antd.css';
 import {Button, Icon, Row} from 'antd';
 
-function Category() {
-  return (
+const Category = (props) => {
+  const {category} = props;
+
+  if (category.categoryTitle === 'noCategory') {
+    return (
     <Row className='category'>
-      <Button type="primary">
-        <Icon type="close" />
-      </Button>
-      <Button className={'category__main-btn'} type="link">
-        Категория
+      <Button className={'category__main-btn category__main-btn--no-category'} type="link">
+        {category.categoryName}
       </Button>
     </Row>
-  );
-}
+    );
+  } else {
+    return (
+      <Row className='category'>
+        <Button type="primary">
+          <Icon type="close" />
+        </Button>
+        <Button className={'category__main-btn'} type="link">
+          {category.categoryName}
+        </Button>
+      </Row>
+    );
+  }
+};
 
 export default Category;
