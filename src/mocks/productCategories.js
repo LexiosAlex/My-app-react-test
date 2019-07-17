1c0547b289ca81d5a344b6d4842dc6220a6ec6e5
LexiosAlex <lexiosma@gmail.com>
2019-07-17 04:42:22 +0300

    created mock data

---
+++ src/mocks/productCategories.js
@@ -0,0 +1,27 @@
+const categories = [
+  {
+    categoryId: 0,
+    categoryName: "Без категории",
+    categoryTitle: "noCategory"
+  },
+  {
+    categoryId: 1,
+    categoryName: "Мясо",
+    categoryTitle: "meat"
+  },
+  {
+    categoryId: 2,
+    categoryName: "Рыба",
+    categoryTitle: "fish"
+  },
+  {
+    categoryId: 3,
+    categoryName: "Овощи",
+    categoryTitle: "vegetables"
+  },
+  {
+    categoryId: 4,
+    categoryName: "Фрукты",
+    categoryTitle: "fruits"
+  }
+];
---
+++ src/mocks/productItems.js
@@ -0,0 +1,146 @@
+const itemsData = [
+  {
+    key: "1",
+    id: "1",
+    name: "Товар 1",
+    wholePrice: "2200",
+    price: "2500",
+    category: "meat",
+    categoryId: 1
+  },
+  {
+    key: "2",
+    id: "2",
+    name: "Товар 2",
+    wholePrice: "2200",
+    price: "2700",
+    category: "meat",
+    categoryId: 1
+  },
+  {
+    key: "3",
+    id: "3",
+    name: "Товар 3",
+    wholePrice: "2200",
+    price: "2700",
+    category: "meat",
+    categoryId: 1
+  },
+  {
+    key: "4",
+    id: "4",
+    name: "Товар 4",
+    wholePrice: "2200",
+    price: "2700",
+    category: "meat",
+    categoryId: 1
+  },
+  {
+    key: "5",
+    id: "5",
+    name: "Товар 1",
+    wholePrice: "2200",
+    price: "2500",
+    category: "fish",
+    categoryId: 2
+  },
+  {
+    key: "6",
+    id: "6",
+    name: "Товар 2",
+    wholePrice: "2200",
+    price: "2700",
+    category: "fish",
+    categoryId: 2
+  },
+  {
+    key: "7",
+    id: "7",
+    name: "Товар 3",
+    wholePrice: "2200",
+    price: "2700",
+    category: "fish",
+    categoryId: 2
+  },
+  {
+    key: "8",
+    id: "8",
+    name: "Товар 4",
+    wholePrice: "2200",
+    price: "2700",
+    category: "fish",
+    categoryId: 2
+  },
+  {
+    key: "9",
+    id: "9",
+    name: "Товар 1",
+    wholePrice: "2200",
+    price: "2500",
+    category: "vegetables",
+    categoryId: 3
+  },
+  {
+    key: "10",
+    id: "10",
+    name: "Товар 2",
+    wholePrice: "2200",
+    price: "2700",
+    category: "vegetables",
+    categoryId: 3
+  },
+  {
+    key: "11",
+    id: "11",
+    name: "Товар 3",
+    wholePrice: "2200",
+    price: "2700",
+    category: "vegetables",
+    categoryId: 3
+  },
+  {
+    key: "12",
+    id: "12",
+    name: "Товар 4",
+    wholePrice: "2200",
+    price: "2700",
+    category: "vegetables",
+    categoryId: 3
+  },
+  {
+    key: "13",
+    id: "13",
+    name: "Товар 1",
+    wholePrice: "2200",
+    price: "2500",
+    category: "fruits",
+    categoryId: 4
+  },
+  {
+    key: "14",
+    id: "14",
+    name: "Товар 2",
+    wholePrice: "2200",
+    price: "2700",
+    category: "fruits",
+    categoryId: 4
+  },
+  {
+    key: "15",
+    id: "15",
+    name: "Товар 3",
+    wholePrice: "2200",
+    price: "2700",
+    category: "fruits",
+    categoryId: 4
+  },
+  {
+    key: "16",
+    id: "16",
+    name: "Товар 4",
+    wholePrice: "2200",
+    price: "2700",
+    category: "fruits",
+    categoryId: 4
+  }
+];
