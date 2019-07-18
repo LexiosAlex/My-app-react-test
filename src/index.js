d06cf0c8d1dca07b8b5ed46a1d46da465499176a
LexiosAlex <lexiosma@gmail.com>
2019-07-18 07:04:33 +0300

    reducer fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./index.css";
 import App from "./components/app/App";
+import { render } from "react-dom";
 import { createStore } from "redux";
 import * as serviceWorker from "./serviceWorker";
 import initialState from "./reducers/mainReducer.js";
 
 const store = createStore(initialState);
 
-const init = () =>
-  ReactDOM.render(
-    <App data={store.getState()} />,
-    document.getElementById("root")
-  );
-
-init();
-store.subscribe(init);
+render(<App data={store.getState()} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
