06c65d71cc4a244f07ed779c4c06c849600a080e
LexiosAlex <lexiosma@gmail.com>
2019-07-25 19:52:29 +0300

    category component refactoring

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -7,29 +7,20 @@ class Category extends React.Component {
   render() {
     const { category, changeCategory, onDeleteCategory } = this.props;
 
-    if (category.categoryTitle === "noCategory") {
-      return (
-        <Row className="category">
-          <Button
-            className="category__main-btn category__main-btn--no-category"
-            type="link"
-            onClick={() => {
-              changeCategory(category.categoryId);
-            }}
-          >
-            {category.categoryName}
-          </Button>
-        </Row>
-      );
-    }
     return (
       <Row className="category">
-        <ModalDeleteCategory
-          onDeleteCategory={onDeleteCategory}
-          id={category.categoryId}
-        />
+        {category.categoryTitle !== "noCategory" && (
+          <ModalDeleteCategory
+            onDeleteCategory={onDeleteCategory}
+            id={category.categoryId}
+          />
+        )}
         <Button
-          className={"category__main-btn"}
+          className={
+            category.categoryTitle === "noCategory"
+              ? "category__main-btn category__main-btn--no-category"
+              : "category__main-btn"
+          }
           type="link"
           onClick={() => {
             changeCategory(category.categoryId);
