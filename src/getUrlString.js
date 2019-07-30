2e1d5764f0470af098ea9c206306294b07e5a12e
LexiosAlex <lexiosma@gmail.com>
2019-07-30 22:15:13 +0300

    removed debugger

--- src/getUrlString.js
+++ src/getUrlString.js
@@ -1,6 +1,5 @@
 const getUrlString = (categoryId, page) => {
   let stringArr = [];
-  debugger;
   stringArr.push(categoryId ? `categoryId=${categoryId}` : ``);
   stringArr.push(page ? `page=${page}` : ``);
   return stringArr.join("&");
