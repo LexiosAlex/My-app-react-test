d5f2dd6e900c054adeca2b5ab7477c2cb230f31a
LexiosAlex <lexiosma@gmail.com>
2019-08-08 17:27:11 +0300

    refactored filteredActiveCategoryAction function

--- src/filterActiveCategoryItems.js
+++ src/filterActiveCategoryItems.js
@@ -1,5 +1,5 @@
 const filterActiveCategoryItems = (categoryId, items) => {
-  return items ? items.filter(it => it.categoryId === categoryId) : []
+  return items ? items.filter(it => it.categoryId === categoryId).map((item, index) => ({...item, key: index})) : []
 };
 
 
