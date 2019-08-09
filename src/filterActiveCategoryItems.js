8e976175af6a7cb8733a24c7cdb04c2881057cd6
LexiosAlex <lexiosma@gmail.com>
2019-08-09 03:44:37 +0300

    removed db

--- src/filterActiveCategoryItems.js
+++ src/filterActiveCategoryItems.js
@@ -2,6 +2,4 @@ const filterActiveCategoryItems = (categoryId, items) => {
   return items ? items.filter(it => it.categoryId === categoryId).map((item, index) => ({...item, key: index})) : []
 };
 
-
-
 export default filterActiveCategoryItems;
