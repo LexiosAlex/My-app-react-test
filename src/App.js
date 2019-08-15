e2a12be9515deb4b0668e604e5151c0a87609ad2
LexiosAlex <lexiosma@gmail.com>
2019-08-15 07:12:34 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import mainPage from "./components/mainPage/mainPage.js"
 import signIn from "./components/signIn/signIn.js"
 
@@ -10,15 +10,19 @@ class App extends Component {
   };
   render() {
     return (
-      <Router>
-        <Route
-          exact path="/login"
-          component={signIn}
-        />
-        <Route
-          exact path="/"
-          component={mainPage} />
-      </Router>
+      <BrowserRouter>
+        <Switch>
+          <Route>
+            <Route
+              exact path="/login"
+              component={signIn}
+            />
+            <Route
+              exact path="/"
+              component={mainPage} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
   )
 }}
 
